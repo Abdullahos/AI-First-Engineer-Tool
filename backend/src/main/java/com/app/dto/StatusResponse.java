fcd93e81098f9aae4f674969fdec9621845177dd
abdullah <aosama@useconsulting.co>
2025-11-26 00:08:55 +0200

    fix: fix returning the result cause can't set fields

--- backend/src/main/java/com/app/dto/StatusResponse.java
+++ backend/src/main/java/com/app/dto/StatusResponse.java
@@ -1,5 +1,10 @@
 package com.app.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class StatusResponse {
     private String status;
     private String errorMessage;
